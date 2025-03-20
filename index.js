4bcb14f8dab2caba3ba38a2661600852a1cf2a87
Hussein <hussein.osman.@student.moringaschool.com>
2025-03-20 08:05:29 +0300

    seve complite cherge

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    return "I'm a named function!";
+  }
+  return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    return "I'm an anonymous function!";
+  };
+}
